c2ce0056c666a2ab9c1b335aedb405e2b464422a
Vincent Jadraque <vincent.jadraque@gmail.com>
2022-04-18 17:18:05 +1000

    Fill out KaseiCoin.sol

--- contracts/KaseiCoin.sol
+++ contracts/KaseiCoin.sol
@@ -9,3 +9,6 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Mintable.sol";
 
 // Create a constructor for the KaseiCoin contract and have the contract inherit the libraries that you imported from OpenZeppelin.
+contract KaseiCoin is ERC20, ERC20Detailed, ERC20Mintable {
+    constructor() ERC20Detailed("KaseiCoin", "KAI", 18) public {}
+}
